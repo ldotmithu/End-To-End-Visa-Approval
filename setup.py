e58f2ec73b0a2a0dca29bdc252358989991a3586
ldotmithu <ldotmithurshan222@gmail.com>
2025-02-11 15:49:05 +0530

    set_up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+REP_NAME = "End-To-End-Visa-Approval"
+author ="ldotmithu"
+
+setup(
+    name='mlproject',
+    version="0.0.0",
+    author=author,
+    author_email='ldotmithurshan222@gmail.com',
+    packages=find_packages()
+)
